9c6d9115f6885b5f85712e5945bd0e6532b4243c
dmn <dim2007vas@gmail.com>
2024-09-05 19:01:37 +0300

    add title to the notes

--- model.py
+++ model.py
@@ -4,10 +4,10 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True)
     password = db.Column(db.String(20))
-
     notes = db.relationship('Notes', backref = 'users')
 
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(30))
     text = db.Column(db.Text)
     created_by = db.Column(db.Integer, db.ForeignKey('users.id'))
